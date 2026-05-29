d6f48153b0434e4154d79eaa07aac636782730a2
leocabelloritz <leo.adolfo.cabello@gmail.com>
2026-05-28 22:15:59 -0300

    Update data.js

--- data.js
+++ data.js
@@ -3,5 +3,6 @@ const CONFIG = {
     'v60': { temp: '90°C - 93°C', pasos: [{nombre: "Infusión", tiempo: 180}] },
     'francesa': { temp: '92°C - 94°C', pasos: [{nombre: "Bloom", tiempo: 30}, {nombre: "Vertido", tiempo: 240}] },
     'aeropress': { temp: '85°C - 90°C', pasos: [{nombre: "Infusión", tiempo: 90}] },
-    'moka': { temp: '80°C', pasos: [] } // Lógica especial
+    'moka': { temp: '80°C', pasos: [] }, // Lógica especial
+    'té verde' : { temp: '70º - 80ºC' , pasos: [{nombre: "Infusión", tiempo: 120}]
 };
